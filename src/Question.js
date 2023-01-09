73267480c4b758c21c23e5ebc629c7867113ced0
Job Alvarez <jobitoalv@gmail.com>
2023-01-09 11:50:43 +1000

    setting onClick fucntion to show info when click

--- src/Question.js
+++ src/Question.js
@@ -6,7 +6,7 @@ const Question = ({title, info}) => {
   return <article className='question'>
             <header>
               <h4>{title}</h4>
-              <button className='btn'>btn</button>
+              <button className='btn'onClick={()=> setShowInfo(!showInfo)}>btn</button>
             </header>
             {
               showInfo && <p>{info}</p>
